8d993ac923bea7fbba803aa664bbc2f205c795e1
Hoang <hoangldph21518@fpt.edu.vn>
2022-06-02 15:22:48 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,8 @@
+const productList = [
+    {id: 1 , name: "Product A", img:"", time:"30", type: "snack" },
+    {id: 1 , name: "Product B", img:"", time:"60", type: "fish" },
+];
+const categories = [
+    {id:1, name :"Category 1"},
+    {id:2, name :"Category 2"}
+];
