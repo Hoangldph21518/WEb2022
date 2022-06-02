69a8c5d72755d3f2fba3beb1962fae0764823436
Hoang <hoangldph21518@fpt.edu.vn>
2022-06-02 15:37:31 +0700

    update

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
-import './style.css'
+import { render } from "./common";
+import { categories, productList } from "./data";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+function showData(data, key) {
+    // kiểm tra
+    if (!Array.isArray(data) || data.length == 0) return false;
+
+    // xử lý
+    let result = "";
+    if (key == "product") {
+        for (let i = 0; i < data.length; i++) {
+            result += `<h2><a href="./detail.html?id=${data[i].id}">${data[i].name}</a></h2>`;
+        }
+    } else {
+        for (let i = 0; i < data.length; i++) {
+            result += `<div><h2><a href="./product.html?id=${data[i].id}">${data[i].name}</a></h2></div>`;
+        }
+    }
+    // trả về kết quả
+    return result;
+}
+
+render("product", showData(productList, "product"));
+render("category", showData(categories, "category"));
